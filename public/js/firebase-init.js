ff8a2a86a427d24ce9ef45c4b91abcfd6e75cff0
Your Name <valechar519@gmail.com>
2024-03-19 13:24:38 +0530

    Describe your changes here

--- public/js/firebase-init.js
+++ public/js/firebase-init.js
@@ -1,13 +1,14 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCiqVDUshhfusWn5Z2b-4p2KVpsyLSNleI",
-    authDomain: "buzzrafters-a3e2b.firebaseapp.com",
-    databaseURL: "https://buzzrafters-a3e2b-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "buzzrafters-a3e2b",
-    storageBucket: "buzzrafters-a3e2b.appspot.com",
-    messagingSenderId: "970830986248",
-    appId: "1:970830986248:web:5d311e15c3031759a5e5bd",
-    measurementId: "G-4W0PT6G2D4"
-  };
+  apiKey: "AIzaSyCiqVDUshhfusWn5Z2b-4p2KVpsyLSNleI",
+  authDomain: "buzzrafters-a3e2b.firebaseapp.com",
+  databaseURL: "https://buzzrafters-a3e2b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "buzzrafters-a3e2b",
+  storageBucket: "buzzrafters-a3e2b.appspot.com",
+  messagingSenderId: "970830986248",
+  appId: "1:970830986248:web:5d311e15c3031759a5e5bd",
+  measurementId: "G-4W0PT6G2D4"
+};
 
-  firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
