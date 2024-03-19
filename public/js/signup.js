e25c8a24b7e39e465b692e55840d336860d01eaf
Your Name <valechar519@gmail.com>
2024-03-19 13:40:47 +0530

    Describe your changes here

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,5 +1,20 @@
-// Wait for the window to load
-window.addEventListener('load', () => {
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCiqVDUshhfusWn5Z2b-4p2KVpsyLSNleI",
+    authDomain: "buzzrafters-a3e2b.firebaseapp.com",
+    databaseURL: "https://buzzrafters-a3e2b-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "buzzrafters-a3e2b",
+    storageBucket: "buzzrafters-a3e2b.appspot.com",
+    messagingSenderId: "970830986248",
+    appId: "1:970830986248:web:5d311e15c3031759a5e5bd",
+    measurementId: "G-4W0PT6G2D4"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  // Wait for the window to load
+  window.addEventListener('load', () => {
     // Get the Firebase Authentication instance
     const auth = firebase.auth();
   
