af2fd21819010f6ca9d206e3f76de3c4cd47b1f1
Your Name <valechar519@gmail.com>
2024-03-20 11:06:35 +0530

    Describe your changes here

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,19 @@
 // Create a script element for the Firebase SDK
 const firebaseScript = document.createElement('script');
-firebaseScript.src = 'https://www.gstatic.com/firebasejs/9.6.0/firebase.js';
+firebaseScript.src = 'https://firebase.google.com/firebase/9.6.0/firebase.js'; // Updated URL
 document.head.appendChild(firebaseScript);
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCiqVDUshhfusWn5Z2b-4p2KVpsyLSNleI",
-  authDomain: "buzzrafters-a3e2b.firebaseapp.com",
-  databaseURL: "https://buzzrafters-a3e2b-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "buzzrafters-a3e2b",
-  storageBucket: "buzzrafters-a3e2b.appspot.com",
-  messagingSenderId: "970830986248",
-  appId: "1:970830986248:web:5d311e15c3031759a5e5bd",
-  measurementId: "G-4W0PT6G2D4"
-};
+    apiKey: "AIzaSyCiqVDUshhfusWn5Z2b-4p2KVpsyLSNleI",
+    authDomain: "buzzrafters-a3e2b.firebaseapp.com",
+    databaseURL: "https://buzzrafters-a3e2b-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "buzzrafters-a3e2b",
+    storageBucket: "buzzrafters-a3e2b.appspot.com",
+    messagingSenderId: "970830986248",
+    appId: "1:970830986248:web:5d311e15c3031759a5e5bd",
+    measurementId: "G-4W0PT6G2D4"
+  };
 
 // Wait for the Firebase SDK to load
 firebaseScript.addEventListener('load', () => {
