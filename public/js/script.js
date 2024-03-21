ba6eee19831e10f7f7721498082ed650afbfbcff
Your Name <valechar519@gmail.com>
2024-03-21 17:35:33 +0530

    Describe your changes here

--- public/js/script.js
+++ public/js/script.js
@@ -5,7 +5,14 @@ import { getDatabase, ref as databaseRef } from 'https://www.gstatic.com/firebas
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // ... (configuration omitted for brevity)
+  apiKey: "AIzaSyCiqVDUshhfusWn5Z2b-4p2KVpsyLSNleI",
+  authDomain: "buzzrafters-a3e2b.firebaseapp.com",
+  databaseURL: "https://buzzrafters-a3e2b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "buzzrafters-a3e2b",
+  storageBucket: "buzzrafters-a3e2b.appspot.com",
+  messagingSenderId: "970830986248",
+  appId: "1:970830986248:web:5d311e15c3031759a5e5bd",
+  measurementId: "G-4W0PT6G2D4"
 };
 
 // Initialize Firebase
