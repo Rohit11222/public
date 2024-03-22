6251719f61f8a6595ab5b6dbbf7956b2fd193e50
Your Name <valechar519@gmail.com>
2024-03-22 17:19:10 +0530

    Exclude node_modules from version control

---
+++ public/js/firebase.js
@@ -0,0 +1,22 @@
+// Import the required Firebase modules
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCiqVDUshhfusWn5Z2b-4p2KVpsyLSNleI",
+  authDomain: "buzzrafters-a3e2b.firebaseapp.com",
+  databaseURL: "https://buzzrafters-a3e2b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "buzzrafters-a3e2b",
+  storageBucket: "buzzrafters-a3e2b.appspot.com",
+  messagingSenderId: "970830986248",
+  appId: "1:970830986248:web:5d311e15c3031759a5e5bd",
+  measurementId: "G-4W0PT6G2D4"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Export the Firebase app instance and auth instance
+export const firebaseApp = firebase.app();
+export const firebaseAuth = firebase.auth();
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,24 +1,8 @@
-// Import the required Firebase modules
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCiqVDUshhfusWn5Z2b-4p2KVpsyLSNleI",
-  authDomain: "buzzrafters-a3e2b.firebaseapp.com",
-  databaseURL: "https://buzzrafters-a3e2b-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "buzzrafters-a3e2b",
-  storageBucket: "buzzrafters-a3e2b.appspot.com",
-  messagingSenderId: "970830986248",
-  appId: "1:970830986248:web:5d311e15c3031759a5e5bd",
-  measurementId: "G-4W0PT6G2D4"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// Import the Firebase app instance and auth instance
+import { firebaseApp, firebaseAuth } from './firebase.js';
 
 // Initialize FirebaseUI
-const ui = new firebaseui.auth.AuthUI(firebase.auth());
+const ui = new firebaseui.auth.AuthUI(firebaseAuth);
 
 // FirebaseUI configuration
 const uiConfig = {
@@ -52,7 +36,7 @@ const signUpElement = document.getElementById('sign-up-element');
 ui.start(signUpElement, uiConfig);
 
 // Listen for auth state changes
-firebase.auth().onAuthStateChanged((user) => {
+firebaseAuth.onAuthStateChanged((user) => {
   if (user) {
     // User is signed in
     console.log('User signed in:', user);
