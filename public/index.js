138bed407dcdfd4b6211804ef42c8a9e38651954
Your Name <valechar519@gmail.com>
2024-03-23 15:56:54 +0530

    Exclude node_modules from version control

--- public/index.js
+++ public/index.js
@@ -1,44 +1,22 @@
-// Import necessary modules
 const express = require('express');
-const path = require('path'); // For working with file paths
-const { auth } = require('express-openid-connect');
-require('dotenv').config(); // For loading environment variables
-
-// Create Express application
+const path = require('path');
 const app = express();
 
-// Configuration for Auth0
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: process.env.SECRET, // Fetch secret from environment variables
-  baseURL: 'https://dev-ddeie1zcfk1vp015.us.auth0.com',
-  clientID: 'j3MKg4otkGpOZpzQyHg9ThYsxy72QVIx',
-  issuerBaseURL: 'https://dev-ddeie1zcfk1vp015.us.auth0.com'
-};
+app.use(express.static('public'));
 
-// Apply Auth0 middleware
-app.use(auth(config));
-
-// Define routes
 app.get('/', (req, res) => {
-  res.send('Welcome to my website');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Route for sign-up page
 app.get('/signup', (req, res) => {
-  // Serve the signup.html page from the public/html directory
-  res.sendFile(path.join(__dirname, 'public', 'html', 'signup.html'));
+  res.sendFile(path.join(__dirname, 'public', 'signup.html'));
 });
 
-// Route for sign-in page
 app.get('/signin', (req, res) => {
-  // Serve the signin.html page from the public/html directory
-  res.sendFile(path.join(__dirname, 'public', 'html', 'signin.html'));
+  res.sendFile(path.join(__dirname, 'public', 'signin.html'));
 });
 
-// Start the server
-// No need to specify port, Netlify will manage this
-app.listen(() => {
-  console.log(`Server is running`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
