669c139d5a08e3d4b58143b602fec4f536cc91aa
Your Name <valechar519@gmail.com>
2024-03-24 18:22:50 +0530

    Exclude node_modules from version control

--- public/js/auth0.js
+++ public/js/auth0.js
@@ -1,26 +1,33 @@
+// Assign the netlifyIdentity object from the global scope
+window.netlifyIdentity = netlifyIdentity;
+
 const userInfoDiv = document.getElementById('userInfo');
 const signInButton = document.getElementById('signInButton');
 const signUpButton = document.getElementById('signUpButton');
 
-window.netlifyIdentity = netlifyIdentity;
+// Initialize the widget with your site's URL
 netlifyIdentity.init({
-  netlifyUrl: 'https://www.buzzrafters.com',
+  netlifyUrl: 'https://buzzrafters.com',
 });
 
+// Handle user login
 netlifyIdentity.on('login', (user) => {
   userInfoDiv.textContent = `Logged in as ${user.user_metadata.full_name}`;
 });
 
+// Handle user logout
 netlifyIdentity.on('logout', () => {
   userInfoDiv.textContent = '';
 });
 
+// Open the login modal
 if (signInButton) {
   signInButton.addEventListener('click', () => {
     netlifyIdentity.open('login');
   });
 }
 
+// Open the signup modal
 if (signUpButton) {
   signUpButton.addEventListener('click', () => {
     netlifyIdentity.open('signup');
